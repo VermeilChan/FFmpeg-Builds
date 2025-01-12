fa5c93010bbe0ed46c1f732ce3755400f56fd68b
Misato <120065660+VermeilChan@users.noreply.github.com>
2025-01-12 22:03:15 +0000

    Update FF_CONFIGURE options to disable unnecessary features and enable MP3 support

--- variants/defaults-gpl.sh
+++ variants/defaults-gpl.sh
@@ -1,4 +1,4 @@
-FF_CONFIGURE="--enable-gpl --enable-version3 --disable-debug"
+FF_CONFIGURE="--enable-gpl --enable-version3 --disable-debug --disable-runtime-cpudetect --disable-swscale-alpha  --disable-ffplay --disable-ffserver --disable-doc --disable-pixelutils --disable-faan --disable-fft --disable-rdft --disable-mdct --disable-lzo  --disable-lsp --disable-error-resilience --disable-dwt --disable-dct --disable-postproc --disable-d3d11va -disable-dxva2 --disable-vaapi --disable-vda --disable-vdpau --disable-everything --enable-encoder=libmp3lame --enable-demuxer=mp3 --enable-muxer=mp3 --disable-parsers --disable-indevs --disable-outdevs --disable-devices --disable-filters"
 FF_CFLAGS=""
 FF_CXXFLAGS=""
 FF_LDFLAGS=""
